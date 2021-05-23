4cfabe663c7db19dc4e7ebac123b265cb0f3e97e
aaaaaaaaaafk <78173356+aaaaaaaaaafk@users.noreply.github.com>
2021-05-23 15:50:47 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import Navbar from './components/navbar'
+import Header from './components/header'
 function App() {
   return (
-  
+  <>
     <Navbar/>
- 
+    <Header/>
+  </>
   );
 }
 
