407d1d9dedf35769d9426593737cd9a98e5beff5
aaaaaaaaaafk <nonuwutisticc@gmail.com>
2021-07-01 15:53:52 -0400

    added technologies page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Head from "./components/Head";
 import Contact from "./components/Contact"
 import Home from "./components/Home";
+import Technologies from "./components/Technologies";
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
         <div className="container-wrap">
           <Head />
           <Home />
+          <Technologies />
           <Contact/>
         </div>
       </div>
---
+++ src/components/Technologies.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Technologies = () => {
+  return (
+    <div id="lara-main">
+      <section id="lara-technologies">
+        <div className="lara-narrow-content">
+          <div className="row">
+            <div
+              className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box fadeInLeft animated"
+              data-animate-effect="fadeInLeft"
+            >
+                <h2 className="sub-heading">Technologies</h2>           
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Technologies;
